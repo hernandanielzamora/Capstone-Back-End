02b6087a782abdbe5333401028db9d63bb8d30dd
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-25 19:17:32 -0400

    -Make sure I have user.id before any actions - Add create method - Add show method - Add destroy - Add set_user method - Add set_reservation method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,37 @@
 class ReservationsController < ApplicationController
+  before_action :set_user
+  before_action :set_reservation, only: [:show, :update, :destroy]
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @reservation
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+  end  
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:reservation_date, :city, :total_cost, :user_id)
+  end
 end
