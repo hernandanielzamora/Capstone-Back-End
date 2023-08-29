49d715d5ecb4bc7c10c813c3ab2b13d2c1deb456
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-29 16:11:04 -0400

    fix rubocop error

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,7 +7,7 @@ class Room < ApplicationRecord
   validates :guest, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :beds, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
-  validates :photo, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
+  validates :photo, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
   validates :cost, presence: true, numericality: { greater_than: 0 }
   validates :reserved, inclusion: { in: [true, false] }
 end
