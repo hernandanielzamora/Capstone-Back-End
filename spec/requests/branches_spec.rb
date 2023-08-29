1fa66bb7f2f37794045f19c9b2d3db1dbb8ca36c
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-29 13:07:58 -0400

    Fix rubocop error

--- spec/requests/branches_spec.rb
+++ spec/requests/branches_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
-RSpec.describe "Branches", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Branches', type: :request do
+  describe 'GET /index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
