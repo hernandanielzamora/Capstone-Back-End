cb5f0baff8ce3f10deb28e25b17aa70e63c8f8b3
Fredo St Fleur <fredostfleur23@gmail.com>
2023-09-04 19:47:19 -0400

    Allow only admin to create new rooms Allow only admin to destroy rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,19 +17,27 @@ class RoomsController < ApplicationController
   end
 
   def create
-    room = Room.new(room_params)
+    if current_user.role == 'admin'
+      room = Room.new(room_params)
 
-    if room.save
-      render json: room, status: :created
+      if room.save
+        render json: room, status: :created
+      else
+        render json: room.errors, status: :unprocessable_entity
+      end
     else
-      render json: room.errors, status: :unprocessable_entity
-    end
+      render json: 'Access not allowed !'
+    end    
   end
 
   def destroy
-    room = Room.find(params[:id])
-    room.destroy
-    head :no_content
+    if current_user.role == 'admin'
+      room = Room.find(params[:id])
+      room.destroy
+      head :no_content
+    else
+      render json: 'Access not allowed !'
+    end    
   end
 
   private
