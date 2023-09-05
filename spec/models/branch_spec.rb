bb22a31e875c4e37e95a72eb183511cdef06a20f
Fredo St Fleur <fredostfleur23@gmail.com>
2023-09-05 19:13:14 -0400

    Update condition to create new branches

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../rails_helper'
 
 RSpec.describe Branch, type: :model do
-  branch_one = Branch.create(city: 'New York')
+  branch_one = Branch.create(city: 'Haiti')
   Room.create(branch: branch_one, name: 'Room One', guest: 2, beds: 1, description: 'This is a room.',
               photo: 'https://www.ikea.com/mx/en/images/products/malm-be102127_pe866548_s5.jpg',
               cost: 100, reserved: false)
